18e4ea1fc598de67e91aea5d958147699e24ecb2
Shreesh2007 <sp0888@srmist.edu.in>
2026-03-25 08:34:12 +0530

    Implement character map for banner display
    Refactored OOPSBannerApp to use a character map for displaying letters.

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,15 +1,70 @@
+import java.util.HashMap;
+
 public class OOPSBannerApp {
-    public static void main(String[] args) {
-        String[] lines = {
-        String.join(" ","  ***  ", "  ***  ", "******  ",     "  ***** "),
-        String.join(" "," ** ** ", " ** ** ", "**   ** ",    " **     "),
-        String.join(" ","**   **","**   **",  "**    **",    "**      "),
-        String.join(" ","**   **", "**   **", "**   ** ",    " **     "),
-        String.join(" ","**   **", "**   **", "******  ",    "  ***   "),
-        String.join(" ","**   **", "**   **", "**      ",    "     ** "),
-        String.join(" "," ** ** ", " ** ** ", "**      ",    "       **"),
-        String.join(" ","  ***  ", "  ***  ", "**      ",    "   ***** ")};
-         for (String line : lines) {
+
+    public static HashMap<Character, String[]> createCharacterMap() {
+        HashMap<Character, String[]> charMap = new HashMap<>();
+
+        charMap.put('O', new String[]{
+            "  ***  ",
+            " ** ** ",
+            "**   **",
+            "**   **",
+            "**   **",
+            "**   **",
+            " ** ** ",
+            "  ***  "
+        });
+
+        charMap.put('P', new String[]{
+            "******  ",
+            "**   ** ",
+            "**   ** ",
+            "******  ",
+            "**      ",
+            "**      ",
+            "**      ",
+            "**      "
+        });
+
+        charMap.put('S', new String[]{
+            "  ***** ",
+            " **     ",
+            "**      ",
+            "  ***   ",
+            "     ** ",
+            "      **",
+            " **   **",
+            "  ***** "
+        });
+
+        return charMap;
+    }
+
+    public static void displayBanner(String message, HashMap<Character, String[]> charMap) {
+        int patternHeight = charMap.get('O').length;
+        String[] lines = new String[patternHeight];
+
+        for (int i = 0; i < patternHeight; i++) {
+            lines[i] = "";
+        }
+
+        for (char ch : message.toCharArray()) {
+            String[] pattern = charMap.get(ch);
+
+            for (int i = 0; i < patternHeight; i++) {
+                lines[i] += pattern[i] + "  ";
+            }
+        }
+
+        for (String line : lines) {
             System.out.println(line);
-        } 
-    }}
+        }
+    }
+
+    public static void main(String[] args) {
+        HashMap<Character, String[]> charMap = createCharacterMap();
+        String message = "OOPS";
+        displayBanner(message, charMap);
+    }
+}
